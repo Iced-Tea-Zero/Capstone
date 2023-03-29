6ffadf7adf0556e4cc872e985389d20a97128a6d
choiwonhee01 <choiwonhee2020@tukorea.ac.kr>
2023-03-29 13:55:11 +0900

    구글 검색창 스크래핑

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 # 크롬 드라이버 실행
@@ -8,7 +9,7 @@ driver = webdriver.Chrome('/path/to/chromedriver')
 # 검색어
 search_query = '디자인 제작 툴'
 
-for page in range(1, 13):
+for page in range(1, 11):
     # 구글 검색 페이지 열기
     url = f'https://www.google.com/search?q={search_query}&start={(page-1)*10}'
     driver.get(url)
@@ -18,11 +19,18 @@ for page in range(1, 13):
     results = driver.find_elements(By.CSS_SELECTOR, 'div.g')
 
     for result in results:
-        link = result.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        # 사이트 제목
         title = result.find_element(By.CSS_SELECTOR, 'h3').text
-        print(title, link)
-        # desc = result.find_element(By.CLASS_NAME, 'Z26q7c UK95Uc').text
-        # print(title, link, desc)
+        # 사이트 링크
+        link = result.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        # 사이트 설명 (만약 태그가 없을 경우, 예외처리하고 넘어가도록 구현)
+        try:
+            desc = result.find_element(By.CLASS_NAME, 'VwiC3b.yXK7lf.MUxGbd.yDYNvb.lyLwlc.lEBKkf').text
+        except NoSuchElementException: 
+            print("Element not found.")
+
+        # 전체 출력
+        print(" ", title, link, desc, sep='\n')
 
 # 브라우저 종료
 time.sleep(10)
