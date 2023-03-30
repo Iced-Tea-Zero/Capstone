1a0b81ed97d2796422453aa9e4666ccf9d8428d8
choiwonhee01 <choiwonhee2020@tukorea.ac.kr>
2023-03-30 19:48:22 +0900

    NLP (파일 -> 단어)

--- NLP2.py
+++ NLP2.py
@@ -3,12 +3,18 @@
 from khaiii import KhaiiiApi
 import pandas as pd
 
-list1 = []
+api = KhaiiiApi()
 
-with open("my_list.txt", "r", encoding="utf-8") as f:
-    my_list = f.readlines()
+for w in api.analyze("안녕하세요. khaiii를 사용해봅시다."):
+    print(w)
 
-list1 = [s.replace('.', '').replace('—', '').replace('·', '').replace('/', '').replace(':', '') for s in map(str, my_list)]
 
-for item in list1:
-    print(item)
+# list1 = []
+#
+# with open("my_list.txt", "r", encoding="utf-8") as f:
+#     my_list = f.readlines()
+#
+# list1 = [s.replace('.', '').replace('—', '').replace('·', '').replace('/', '').replace(':', '') for s in map(str, my_list)]
+#
+# for item in list1:
+#     print(item)
