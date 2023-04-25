e236ada7aa1a34df0350c14f7d1d9be9d3b4e634
zeroju <yeongju0922@naver.com>
2023-04-25 14:42:49 +0900

    feat : Modify list, detail page

--- front/js/detail.js
+++ front/js/detail.js
@@ -16,6 +16,19 @@ img1을 보여줌*/ $("#img2").click(function () {
 $(function () {
   let num = 0;
 
+  $("#pic").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+
   $("#pic1").click(function () {
     if (num == 0) {
       $(this).attr("src", "../data/img/star2.png");
@@ -162,6 +175,354 @@ $(function () {
 $(function () {
   let num = 0;
   $("#pic13").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic14").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic15").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic16").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic17").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic18").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic19").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic20").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic21").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic22").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic23").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic24").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic25").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic26").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic27").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic28").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic29").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic30").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic31").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic32").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic33").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic34").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic35").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic36").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic37").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic38").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic39").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic40").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic41").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic42").click(function () {
     if (num == 0) {
       $(this).attr("src", "../data/img/star2.png");
       num = 1;
--- front/js/list.js
+++ front/js/list.js
@@ -1,3 +1,25 @@
+// 순위나열 데이터 들어오면 내림차순으로 정렬
+// const arr = [2, 1, 3, 10];
+
+// arr.sort(function (a, b) {
+//   if (a > b) return 1;
+//   if (a === b) return 0;
+//   if (a < b) return -1;
+// });
+// document.writeln(arr + "<br>"); // [1, 2, 3, 10]
+$(function () {
+  let num = 0;
+
+  $("#pic").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
 $(function () {
   let num = 0;
 
@@ -156,13 +178,351 @@ $(function () {
     }
   });
 });
-
-// 순위나열 데이터 들어오면 내림차순으로 정렬
-// const arr = [2, 1, 3, 10];
-
-// arr.sort(function (a, b) {
-//   if (a > b) return 1;
-//   if (a === b) return 0;
-//   if (a < b) return -1;
-// });
-// document.writeln(arr + "<br>"); // [1, 2, 3, 10]
+$(function () {
+  let num = 0;
+  $("#pic14").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic15").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic16").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic17").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic18").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic19").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic20").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic21").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic22").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic23").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic24").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic25").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic26").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic27").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic28").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic29").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic30").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic31").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic32").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic33").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic34").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic35").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic36").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic37").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic38").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic39").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic40").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic41").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
+$(function () {
+  let num = 0;
+  $("#pic42").click(function () {
+    if (num == 0) {
+      $(this).attr("src", "../data/img/star2.png");
+      num = 1;
+    } else {
+      $(this).attr("src", "../data/img/star1.png");
+      num = 0;
+    }
+  });
+});
